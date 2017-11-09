84f0619b5aaecc191c15f8327a9eedffe9e0e087
SarahLKatz <soshasarah@gmail.com>
2017-11-09 15:19:01 -0500

    Update NewRace.js

--- client/components/NewRace.js
+++ client/components/NewRace.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-// import store from '../store';
-// import axios from 'axios';
 import {connect} from 'react-redux'
 import {createRaceThunk} from '../store'
 
